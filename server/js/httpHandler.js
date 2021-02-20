fede47a25a1e1a76fea3e2277692cf4ecce82e47
wapu-lab <nieruoxi@hotmail.com>
2021-02-20 14:48:37 -0600

    Complete background image functionality

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -16,18 +16,41 @@ module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
-  res.writeHead(200, headers);
+  if (req.method === 'OPTIONS' && req.url === '/') {
+    res.writeHead(200, headers);
+    res.end();
+    next();
+  }
 
-  if (req.method === 'GET') {
+  if (req.method === 'GET' && req.url === '/') {
     let command = messageQueue.dequeue();
     if(command) {
+      res.writeHead(200, headers);
       res.end(command);
+      next();
     } else {
+      res.writeHead(200, headers);
       res.end(generateRandomCommand());
+      next();
     }
   }
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
+
+  if (req.method === 'GET' && req.url === '/background.jpg') {
+    console.log('WE ARE GETTING IN');
+    fs.readFile(module.exports.backgroundImageFile, (err, data) => {
+      if (err) {
+        res.writeHead(404, headers);
+        res.end();
+        next();
+      } else {
+        res.writeHead(200, headers);
+        res.end(data);
+        next();
+      }
+    });
+
+  }
+ // invoke next() at the end of a request to help with testing!
 };
 
 // helper function
